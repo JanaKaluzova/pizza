0aa62b66951db4eb9eff475e31e21ea8a49cd063
Radek Kaluža <kaluza.radek@gmail.com>
2022-08-29 11:39:22 +0200

    useEffect for closing cart in DOM

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import CartCSS from '../components/Cart.module.css'
 import { FiShoppingCart } from 'react-icons/fi'
 import { AppStateContext } from './AppState'
@@ -6,11 +6,25 @@ import { AppStateContext } from './AppState'
 type Props = {}
 
 export const Cart: React.FC<Props> = () => {
+  const containerRef = useRef<HTMLDivElement | null>(null)
   const [isOpen, setIsOpen] = useState(false)
 
   const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     setIsOpen((prevState) => !prevState)
   }
+
+  useEffect(() => {
+    document.addEventListener('mousedown', (e) => {
+      if (containerRef.current && !containerRef.current.contains(e.target as Node)) {
+        setIsOpen(false)
+      }
+      return () => {
+        document.removeEventListener('mousedown', () => {
+          setIsOpen(false)
+        })
+      }
+    })
+  })
 
   return (
     <AppStateContext.Consumer>
@@ -19,7 +33,7 @@ export const Cart: React.FC<Props> = () => {
           return sum + item.quantity
         }, 0)
         return (
-          <div className={CartCSS.cartContainer}>
+          <div className={CartCSS.cartContainer} ref={containerRef}>
             <button className={CartCSS.button} type="button" onClick={handleClick}>
               <FiShoppingCart />
               <span>{itemsCount} pizza(s)</span>
